feeb9dd727377e7a9cc7aad44f72e2c9f560852d
Stephen <Stephen@DESKTOP-68GODKE>
2020-05-26 13:27:05 -0400

    Made the player dot smaller, and minor improvements to the color code

--- Master_karel.quest/karelQuest/Draw.java
+++ Master_karel.quest/karelQuest/Draw.java
@@ -7,7 +7,9 @@ import javax.swing.*;
 public class Draw extends JPanel{
 	//THIS IS JUST FOR TESTING
 	Floor f;
-	
+	Color defPlayerColor = new Color(255, 77, 0);
+	Color defBackgroundColor = new Color(209, 209, 224);
+	Color defWallColor = new Color(118, 118, 162);
 	public void Drawing(Floor floor) {
 		//PRE: a Floor object
 		//POST: Draws it to a frame (Make sure you add it to a frame with FRAMENAME.add(DRAW_OBJECT_NAME))
@@ -36,18 +38,24 @@ public class Draw extends JPanel{
 					//If the space is walkable
 					if(f.getAt(j, i).hasEntity()) {
 						//If the space is a player
-						g.setColor(new Color(255, 77, 0));
+						
+						//Removes background color from player
+						g.setColor(defBackgroundColor);
 						g.fillRect(i*10, j*10, 10, 10);
+						
+						//Makes player
+						g.setColor(defPlayerColor);
+						g.fillRect(i*10+2, j*10+2, 6, 6);
 					}
 					else {
-						g.setColor(new Color(209, 209, 224));
+						g.setColor(defBackgroundColor);
 						g.fillRect(i*10, j*10, 10, 10);
 					}
 					
 				}
 				
 				else {
-					g.setColor(new Color(118, 118, 162));
+					g.setColor(defWallColor);
 					g.fillRect(i*10, j*10, 10, 10);
 				}
 				
