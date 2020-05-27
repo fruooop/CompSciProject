4fca2bdc3a65ea85662e8eb4bd4da85fe9e6c5ca
Sam Silliman <Sam Silliman@Sam-PC>
2020-05-27 11:14:04 -0400

    changed up pathfinding so that the monster

--- Master_karel.quest/karelQuest/Monster.java
+++ Master_karel.quest/karelQuest/Monster.java
@@ -4,17 +4,42 @@ public abstract class Monster extends Entity
 {
 	private int baseDamage;
 	private int damageRange;
+	private int range;
+	private int moves;
 	
-	public Monster(int sendHealth, int newX, int newY, String newString, int newBase, int newRange)
+	public Monster(int sendHealth, int newX, int newY, String newString, int newBase, int newDamageRange, int newRange, int newMoves)
 	{
 		super(sendHealth, newX, newY, newString);
 		baseDamage = newBase;
-		damageRange = newRange;
+		damageRange = newDamageRange;
+		range= newRange;
+		moves = newMoves;			
 	}
-	public void act() {}
+	//if the monster and the player occupy the same room, the mosnter will move into range and attack the player if they are close enough 
+	//pre: floor has rooms & monster and player are on the same floor
+	//post: the monster will move towards the player and attempt to attack
+	public void act(Floor f, Entity player) 
+	{
+		int pX = player.getX(); //player positions
+		int pY = player.getY();
+		int mX = getX(); //monster position
+		int mY = getY();
+		if(f.getRoomAt(mX, mY).equals(f.getRoomAt(pX, pY)))//they are in the same room
+		{
+			int numMoves = moves;
+			while(moves > 0 && !isInRange(player))
+			{
+				move(f, player);
+				numMoves--;
+			}
+			if(isInRange(player))
+			{
+				attack(player);
+			}
+		}
+	}
 	//monster will move to minimize the distance between the monster and player
 	//pre: distance between monster and player >1 (the tiles are not adjacent)
-	//aaaaaaaaaaaaaaaaaaaaaaaaa
 	public void move(Floor f, Entity player)
 	{
 		int pX = player.getX(); //player positions
@@ -43,6 +68,11 @@ public abstract class Monster extends Entity
 				swap(mX, mY+1, f);
 			}
 		}
+	}
+	//the player is close enough to attack
+	public boolean isInRange(Entity player)
+	{
+		return Math.abs(getX() - player.getX()) < range + 1 && Math.abs(getY() - player.getY()) < range + 1;
 	}
 	public void attack(Entity player) 
 	{
