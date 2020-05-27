edf075ec24bc5407485154214167d71363ce5046
Stephen <Stephen@10.0.1.75>
2020-05-27 08:25:22 -0400

    Honestly, I don't remember what I changed. This was from last night.

--- Master_karel.quest/karelQuest/Draw.java
+++ Master_karel.quest/karelQuest/Draw.java
@@ -7,9 +7,12 @@ import javax.swing.*;
 public class Draw extends JPanel{
 	//THIS IS JUST FOR TESTING
 	Floor f;
+	Color BLACK = new Color(0,0,0);
 	Color defPlayerColor = new Color(255, 77, 0);
 	Color defBackgroundColor = new Color(209, 209, 224);
 	Color defWallColor = new Color(118, 118, 162);
+	Color defZombieColor = new Color(0, 255, 102);
+	Color defSkeletonColor = new Color(65, 85, 95);
 	public void Drawing(Floor floor) {
 		//PRE: a Floor object
 		//POST: Draws it to a frame (Make sure you add it to a frame with FRAMENAME.add(DRAW_OBJECT_NAME))
@@ -34,15 +37,38 @@ public class Draw extends JPanel{
 				if(f.getAt(j, i).isWalkable()) {
 					//If the space is walkable
 					if(f.getAt(j, i).hasEntity()) {
-						//If the space is a player
+						//If the space is an entity
+						if(f.getAt(j,i).getEntity().getName().toLowerCase().contains("player")) {
+							//Removes background color from player
+							g.setColor(defBackgroundColor);
+							g.fillRect(i*10, j*10, 10, 10);
+							
+							//Makes player
+							g.setColor(defPlayerColor);
+							g.fillRect(i*10+2, j*10+2, 6, 6);
+						}
 						
-						//Removes background color from player
-						g.setColor(defBackgroundColor);
-						g.fillRect(i*10, j*10, 10, 10);
+						if(f.getAt(j,i).getEntity().getName().toLowerCase().contains("skeleton")) {
+							//Removes background color from player
+							g.setColor(defBackgroundColor);
+							g.fillRect(i*10, j*10, 10, 10);
+							
+							//Makes player
+							g.setColor(defSkeletonColor);
+							g.fillRect(i*10+2, j*10+2, 6, 6);
+						}
 						
-						//Makes player
-						g.setColor(defPlayerColor);
-						g.fillRect(i*10+2, j*10+2, 6, 6);
+						if(f.getAt(j,i).getEntity().getName().toLowerCase().contains("zombie")) {
+							//Removes background color from player
+							g.setColor(defBackgroundColor);
+							g.fillRect(i*10, j*10, 10, 10);
+							
+							//Makes player
+							g.setColor(defZombieColor);
+							g.fillRect(i*10+2, j*10+2, 6, 6);
+						}
+						
+						
 					}
 					else {
 						g.setColor(defBackgroundColor);
