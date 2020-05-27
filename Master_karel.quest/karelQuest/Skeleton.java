7129bdae27255e8ec2075867cbfa6f26382c49c3
Henry <Henry@PINCUSHION>
2020-05-27 09:51:12 -0400

    test

--- Master_karel.quest/karelQuest/Skeleton.java
+++ Master_karel.quest/karelQuest/Skeleton.java
@@ -7,7 +7,7 @@ public class Skeleton extends Monster
 		super(sendHealth, newX, newY, newString, newBase, newRange);
 	}
 	//skeltons have a bow, so they can attack from two blocks away
-	//since they have a ranged attack they can only move once
+	//since they have a ranged attack they can only move once test
 	public void act(Floor f, Entity p)
 	{
 		int pX = p.getX();
