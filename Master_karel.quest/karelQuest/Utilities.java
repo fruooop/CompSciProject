a070ff176849a8df2bfd471950d0c4728e1df126
Henry <henriotto@gmail.com>
2020-06-01 17:41:43 -0400

    adjectives

--- Master_karel.quest/karelQuest/Utilities.java
+++ Master_karel.quest/karelQuest/Utilities.java
@@ -50,49 +50,21 @@ public class Utilities {
 	}
 	
 	public static String randomPosAdj() {
-		//returns a random positive adjective. https://www.thesaurus.com/browse/good?s=t.
-		int random = (int)(Math.random()*40);
+		//returns a random adjective that is a synonym for happy. https://www.thesaurus.com/browse/good?s=t.
+		int random = (int)(Math.random()*12);
 		switch(random) {
 			case 0: return "acceptable";
-			case 1: return "Jason";
-			case 2: return "Christopher";
-			case 3: return "James";
-			case 4: return "David";
-			case 5: return "Robert";
-			case 6: return "John";
-			case 7: return "Brian";
-			case 8: return "Matthew";
-			case 9: return "William";
-			case 10: return "Daniel";
-			case 11: return "Joseph";
-			case 12: return "Eric";
-			case 13: return "Jeffrey";
-			case 14: return "Kevin";
-			case 15: return "Richard";
-			case 16: return "Scott";
-			case 17: return "Steven";
-			case 18: return "Mark";
-			case 19: return "Thomas";
-			case 20: return "Jennifer";
-			case 21: return "Amy";
-			case 22: return "Heather";
-			case 23: return "Melissa";
-			case 24: return "Angela";
-			case 25: return "Michelle";
-			case 26: return "Kimberly";
-			case 27: return "Lisa";
-			case 28: return "Stephanie";
-			case 29: return "Nicole";
-			case 30: return "Rebecca";
-			case 31: return "Christina";
-			case 32: return "Jessica";
-			case 33: return "Amanda";
-			case 34: return "Elizabeth";
-			case 35: return "Kelly";
-			case 36: return "Sarah";
-			case 37: return "Julie";
-			case 38: return "Mary";
-			case 39: return "Laura";
+			case 1: return "excellent";
+			case 2: return "exceptional";
+			case 3: return "favorable";
+			case 4: return "great";
+			case 5: return "marvelous";
+			case 6: return "positive";
+			case 7: return "satisfactory";
+			case 8: return "satisfying";
+			case 9: return "superb";
+			case 10: return "valuable";
+			case 11: return "wonderful";
 			default: return "You Messed Up";
 		}
 	}
