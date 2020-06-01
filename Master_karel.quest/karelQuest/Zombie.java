06ac88c2d6b4ae0019258b0dd4ff7bbca9d969b1
Henry <Henry@PINCUSHION>
2020-06-01 14:07:41 -0400

    makes zomb deal damgage

--- Master_karel.quest/karelQuest/Zombie.java
+++ Master_karel.quest/karelQuest/Zombie.java
@@ -5,7 +5,7 @@ public class Zombie extends Monster
 	public Zombie(int sendHealth, int newX, int newY, String newString)
 	{
 		//As of now, zombies have a base damage of 0, a damage range of 0, an attack range of 1, and a vision range of 6.
-		super(sendHealth, newX, newY, newString, 0, 0, 1, 6);
+		super(sendHealth, newX, newY, newString, 5, 5, 1, 6);
 		
 	}
 }
