549ed0ed78d662e1f44efcb274de94e2dc97d2fa
Henry <Henry@PINCUSHION>
2020-06-01 09:24:34 -0400

    Made comments for skeles and zombs more explanatory

--- Master_karel.quest/karelQuest/Skeleton.java
+++ Master_karel.quest/karelQuest/Skeleton.java
@@ -4,6 +4,7 @@ public class Skeleton extends Monster
 {
 	public Skeleton(int sendHealth, int newX, int newY, String newString)
 	{
+		//As of now, zombies have a base damage of 0, a damage range of 0, an attack range of 2, and a vision range of 4.
 		super(sendHealth, newX, newY, newString, 0, 0, 2, 4);
 	}
 }
